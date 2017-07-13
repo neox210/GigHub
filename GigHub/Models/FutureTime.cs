e89b88838bd5b14c0deef4e2b18cce7808e44a19
Łukasz Brze <neox210@gmail.com>
2017-07-13 13:34:31 +0200

    initial commit

---
+++ GigHub/Models/FutureTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.Models
+{
+    public class ValidDate : ValidationAttribute
+    {
+
+        public override bool IsValid(object value)
+        {
+            DateTime dateTime;
+
+            var isValid = DateTime.TryParseExact(Convert.ToString(value),
+                "HH:mm",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out dateTime);
+
+            return (isValid);
+        }
+
+    }
+}
